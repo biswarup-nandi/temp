0013836cde084df9a73a50428eee491346e13dc4
bnandi <biswarup1010@hotmail.com>
2024-08-01 22:41:57 +0530

    Changed

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Load JSON configuration
+config_file="config.json"
+
+# Extract variables and construct the variables string
+variables=$(jq -r '
+    .work.variables |
+    to_entries |
+    map(
+        .key + "=" + (
+            if .value | type == "object" then
+                (.value | to_entries | map(
+                    .key + "=" + (.value | tostring)
+                ) | join(","))
+            else
+                (.value | tostring)
+            end
+        )
+    ) |
+    join(",")
+' $config_file)
+
+# Run the databricks bundle command
+databricks bundle run --var="$variables"
