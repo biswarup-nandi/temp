895281eebc4fabb452e9108273063ebc5cfff832
bnandi <biswarup1010@hotmail.com>
2024-08-02 12:27:00 +0530

    changed shell

--- script.sh
+++ script.sh
@@ -4,22 +4,7 @@
 config_file="config.json"
 
 # Extract variables and construct the variables string
-variables=$(jq -r '
-    .work.variables |
-    to_entries |
-    map(
-        .key + "=" + (
-            if .value | type == "object" then
-                (.value | to_entries | map(
-                    .key + "=" + (.value | tostring)
-                ) | join(","))
-            else
-                (.value | tostring)
-            end
-        )
-    ) |
-    join(",")
-' $config_file)
+variables=$(jq -r '.work.variables | to_entries | map(.key + "=" + (if .value | type == "object" then (.value | to_entries | map(.key + "=" + (.value | tostring)) | join(",")) else (.value | tostring) end)) | join(",")' "$config_file")
 
 # Run the databricks bundle command
 databricks bundle run --var="$variables"
